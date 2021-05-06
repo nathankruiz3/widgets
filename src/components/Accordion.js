ca1b0fc24250355038eedee3f90e7cce833cd65b
nathan ruiz <nathankruiz3@gmail.com>
2021-05-06 14:08:11 -0700

    Finished Accordion component

---
+++ src/components/Accordion.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Accordion = (props) => {
+  const [activeTitle, setActiveTitle] = useState(null);
+
+  const onTitleClick = (index) => {
+    activeTitle === index ? setActiveTitle(null) : setActiveTitle(index);
+  };
+
+  return (
+    <div className="ui styled accordion">
+      {props.items &&
+        props.items.map((item, index) => {
+          return (
+            <React.Fragment key={item.title}>
+              <div
+                className={activeTitle === index ? "title active" : "title"}
+                onClick={() => onTitleClick(index)}
+              >
+                <i className="dropdown icon"></i>
+                {item.title}
+              </div>
+              <div
+                className={activeTitle === index ? "content active" : "content"}
+              >
+                {item.content}
+              </div>
+            </React.Fragment>
+          );
+        })}
+    </div>
+  );
+};
+
+export default Accordion;
