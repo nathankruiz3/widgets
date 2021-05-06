c2b6061cd552217abfc5a9cd88636b889e43d6b4
nathan ruiz <nathankruiz3@gmail.com>
2021-05-06 14:07:57 -0700

    rendered the Accordion component in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content:
+      "React is an open-source, front-end, JS library for building UI and UI components. It can be used as a base in the development of single-page applications.",
+  },
+  {
+    title: "Why use React?",
+    content:
+      "React is a favorite among engineers. It's simple, lightweight, and has a large support community.",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating reuseable components.",
+  },
+];
 
 const App = () => {
-  return <div style={{ textAlign: "center" }}>App</div>;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Accordion items={items} />
+    </div>
+  );
 };
 
 export default App;
